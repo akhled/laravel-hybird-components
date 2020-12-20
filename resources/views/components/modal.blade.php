82b8f5c1a0016448179d44b89c20fd8fd6d2f4e2
akhaled <hi@amrography.com>
2020-12-20 17:07:52 +0200

    :art: Fix modal isset checks

--- resources/views/components/modal.blade.php
+++ resources/views/components/modal.blade.php
@@ -35,7 +35,7 @@ break;
 @endphp
 
 <div x-data="{
-    showModal: {{ $openOnInit }}
+    showModal: {{ $openOnInit ? 'true' : 'false' }}
 }">
 
     {{ $slot }}
@@ -49,49 +49,59 @@ break;
         <div x-show="showModal"
             class="fixed inset-0 transform transition-all"
             x-on:click="showModal = false"
-            {{-- x-transition:enter="ease-out duration-100"
+            {{--
+            x-transition:enter="ease-out duration-100"
             x-transition:enter-start="opacity-0"
             x-transition:enter-end="opacity-100"
             x-transition:leave="ease-in duration-200"
             x-transition:leave-start="opacity-100"
-            x-transition:leave-end="opacity-0" --}}
-            >
+            x-transition:leave-end="opacity-0"
+            --}}>
             <div class="absolute inset-0 bg-gray-500 opacity-75"></div>
         </div>
 
         <div x-show="showModal"
             class="bg-white rounded-lg overflow-hidden shadow-xl transform transition-all sm:w-full {{ $maxWidth }}"
-            {{-- x-transition:enter="ease-out duration-300"
+            {{--
+            x-transition:enter="ease-out duration-300"
             x-transition:enter-start="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
             x-transition:enter-end="opacity-100 translate-y-0 sm:scale-100"
             x-transition:leave="ease-in duration-200"
             x-transition:leave-start="opacity-100 translate-y-0 sm:scale-100"
-            x-transition:leave-end="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95" --}}
-            >
+            x-transition:leave-end="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+            --}}>
 
             <div class="flex justify-between items-center pt-5 px-4">
-                <h3 class="text-lg">
-                    {{ $title }}
-                </h3>
+                @isset($title)
+                    <h3 class="text-lg">
+                        {{ $title }}
+                    </h3>
+                @endisset
+                @endif
                 @if ($times)
                     <x-hybrid-button-modal-times />
                 @endif
             </div>
 
-            <div class="bg-white px-4 py-4 sm:p-6 sm:pb-4 mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
-                {{ $content }}
-            </div>
+            @isset($content)
+                <div class="bg-white px-4 py-4 sm:p-6 sm:pb-4 mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                    {{ $content }}
+                </div>
+            @endisset
 
-            @if ($footer || $cancel || $confirm)
+            @if (isset($footer) || $cancel || $confirm)
                 <div class="px-6 py-4 bg-gray-100 text-right">
-                    {{ $footer }}
+                    @isset($footer)
+                        {{ $footer }}
+                    @endisset
 
                     @if ($confirm)
                         <x-hybrid-button color="blue">Confirm</x-hybrid-button>
                     @endif
 
                     @if ($cancel)
-                        <x-hybrid-button color="red" x-on:click="showModal = false">Cancel</x-hybrid-button>
+                        <x-hybrid-button color="red"
+                            x-on:click="showModal = false">Cancel</x-hybrid-button>
                     @endif
                 </div>
             @endif

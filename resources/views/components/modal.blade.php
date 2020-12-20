3539d0269f6840349accedf5124dede70143a111
akhaled <hi@amrography.com>
2020-12-20 16:49:29 +0200

    :zap: Push to packgist

--- resources/views/components/modal.blade.php
+++ resources/views/components/modal.blade.php
@@ -10,6 +10,7 @@
 @todo Confirm color and text button
 @todo Cancel color and text button
 @todo What to do with confirm button ?
+@todo Transition usage
 --}}
 
 @php
@@ -48,23 +49,25 @@ break;
         <div x-show="showModal"
             class="fixed inset-0 transform transition-all"
             x-on:click="showModal = false"
-            x-transition:enter="ease-out duration-100"
+            {{-- x-transition:enter="ease-out duration-100"
             x-transition:enter-start="opacity-0"
             x-transition:enter-end="opacity-100"
             x-transition:leave="ease-in duration-200"
             x-transition:leave-start="opacity-100"
-            x-transition:leave-end="opacity-0">
+            x-transition:leave-end="opacity-0" --}}
+            >
             <div class="absolute inset-0 bg-gray-500 opacity-75"></div>
         </div>
 
         <div x-show="showModal"
             class="bg-white rounded-lg overflow-hidden shadow-xl transform transition-all sm:w-full {{ $maxWidth }}"
-            x-transition:enter="ease-out duration-300"
+            {{-- x-transition:enter="ease-out duration-300"
             x-transition:enter-start="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
             x-transition:enter-end="opacity-100 translate-y-0 sm:scale-100"
             x-transition:leave="ease-in duration-200"
             x-transition:leave-start="opacity-100 translate-y-0 sm:scale-100"
-            x-transition:leave-end="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95">
+            x-transition:leave-end="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95" --}}
+            >
 
             <div class="flex justify-between items-center pt-5 px-4">
                 <h3 class="text-lg">
@@ -83,12 +86,12 @@ break;
                 <div class="px-6 py-4 bg-gray-100 text-right">
                     {{ $footer }}
 
-                    @if ($cancel)
-                        <x-hybrid-button color="red" x-on:click="showModal = false">Cancel</x-hybrid-button>
-                    @endif
-
                     @if ($confirm)
                         <x-hybrid-button color="blue">Confirm</x-hybrid-button>
+                    @endif
+
+                    @if ($cancel)
+                        <x-hybrid-button color="red" x-on:click="showModal = false">Cancel</x-hybrid-button>
                     @endif
                 </div>
             @endif
